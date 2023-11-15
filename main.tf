a155ac298331bba23514521755944cc3d083a424
rhoribe <rhoribe1908@gmail.com>
2023-11-15 17:36:01 -0300

    test

--- main.tf
+++ main.tf
@@ -7,14 +7,7 @@ resource "aws_s3_bucket" "example" {
   }
 }
 
-resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
 
-  tags = {
-    Name = "main"
-  }
-}
 
 terraform {
   backend "s3" {
@@ -26,3 +19,27 @@ terraform {
 }
 
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "c6a.2large"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
