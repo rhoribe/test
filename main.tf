e2503262d719e73edb5f56a19b03bad5c27855c6
rhoribe <rhoribe1908@gmail.com>
2023-11-15 17:04:44 -0300

    test

--- main.tf
+++ main.tf
@@ -27,29 +27,29 @@ terraform {
 }
 
 
-data "aws_ami" "this" {
-  most_recent = true
-  owners      = ["amazon"]
-  filter {
-    name   = "architecture"
-    values = ["arm64"]
-  }
-  filter {
-    name   = "name"
-    values = ["al2023-ami-2023*"]
-  }
-}
+# data "aws_ami" "this" {
+#   most_recent = true
+#   owners      = ["amazon"]
+#   filter {
+#     name   = "architecture"
+#     values = ["arm64"]
+#   }
+#   filter {
+#     name   = "name"
+#     values = ["al2023-ami-2023*"]
+#   }
+# }
 
 
-resource "aws_instance" "this" {
-  ami = data.aws_ami.this.id
-  instance_market_options {
-    spot_options {
-      max_price = 0.0031
-    }
-  }
-  instance_type = "t4g.nano"
-  tags = {
-    Name = "test-spot"
-  }
-}
+# resource "aws_instance" "this" {
+#   ami = data.aws_ami.this.id
+#   instance_market_options {
+#     spot_options {
+#       max_price = 0.0031
+#     }
+#   }
+#   instance_type = "t4g.nano"
+#   tags = {
+#     Name = "test-spot"
+#   }
+# }
