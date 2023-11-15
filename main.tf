28749156e03bd5dd9cca149530bdb2f0e97c16c4
rhoribe <rhoribe1908@gmail.com>
2023-11-15 17:39:31 -0300

    test

--- main.tf
+++ main.tf
@@ -17,29 +17,3 @@ terraform {
     encrypt        = true
   }
 }
-
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "c6a.2large"
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
