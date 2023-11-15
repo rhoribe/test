d0839d8525f919866bd100e646b3e727352bc086
rhoribe <rhoribe1908@gmail.com>
2023-11-15 17:01:47 -0300

    test

--- main.tf
+++ main.tf
@@ -25,3 +25,30 @@ terraform {
     encrypt        = true
   }
 }
+
+
+data "aws_ami" "this" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "architecture"
+    values = ["arm64"]
+  }
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*"]
+  }
+}
+
+resource "aws_instance" "this" {
+  ami = data.aws_ami.this.id
+  instance_market_options {
+    spot_options {
+      max_price = 0.0031
+    }
+  }
+  instance_type = "t4g.nano"
+  tags = {
+    Name = "test-spot"
+  }
+}
