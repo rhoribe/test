ca29bac49a646288d2c3cc311ce94ff4fb104ae8
rhoribe <rhoribe1908@gmail.com>
2023-11-15 17:21:59 -0300

    test

--- main.tf
+++ main.tf
@@ -7,15 +7,6 @@ resource "aws_s3_bucket" "example" {
   }
 }
 
-resource "aws_s3_bucket" "example1" {
-  bucket = "rhoribe-test-bucket1"
-
-  tags = {
-    Name        = "rhoribe-test-bucket1"
-    Environment = "Dev"
-  }
-}
-
 
 terraform {
   backend "s3" {
@@ -27,29 +18,3 @@ terraform {
 }
 
 
-data "aws_ami" "this" {
-  most_recent = true
-  owners      = ["amazon"]
-  filter {
-    name   = "architecture"
-    values = ["arm64"]
-  }
-  filter {
-    name   = "name"
-    values = ["al2023-ami-2023*"]
-  }
-}
-
-
-resource "aws_instance" "this" {
-  ami = data.aws_ami.this.id
-  instance_market_options {
-    spot_options {
-      max_price = 0.0031
-    }
-  }
-  instance_type = "t4g.nano"
-  tags = {
-    Name = "test-spot"
-  }
-}
